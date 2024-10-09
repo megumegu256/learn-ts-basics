981a819682c90d5948df54b2983dcff60c772119
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-10-09 21:41:56 +0900

    update prac01.js and prac01.ts

--- dist/prac01.js
+++ dist/prac01.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const userName = "Bob";
-console.log(`Hey, ${userName}.`);
+const name = "TypeScriptの勉強";
+const priority = 3;
+console.log(`Todo 1 => ${name}（優先度:${priority})`); // テンプレート文字列
--- src/prac01.ts
+++ src/prac01.ts
@@ -1,6 +1,6 @@
 export {};
-// 文字列型 (string type) の変数 name の宣言と初期化
-let name: string = "TypeScriptの勉強";
-// 数値型 (number type) の変数 priority の宣言と初期化
-let priority: number = 3;
-console.log(name, priority);
+const todo = {
+  name: "TypeScriptの勉強", // name = "..." ではない点に要注意
+  priority: 3, // priority = "..." ではない点に要注意
+};
+console.log(`Todo 1 => ${todo.name}（優先度:${todo.priority})`);
