c43ff5001c88cd8e23d530704b8de59a32891291
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-10-09 21:26:45 +0900

    update prac01.ts

--- src/prac01.ts
+++ src/prac01.ts
@@ -1,4 +1,6 @@
 export {};
-
-const userName: string = "Bob";
-console.log(`Hey, ${userName}.`);
+// 文字列型 (string type) の変数 name の宣言と初期化
+let name: string = "TypeScriptの勉強";
+// 数値型 (number type) の変数 priority の宣言と初期化
+let priority: number = 3;
+console.log(name, priority);
