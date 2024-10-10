41a0424b530775b3c4741ea77f8477bb6fee51be
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-10-10 09:47:19 +0900

    create 2 files and update 2 files

--- src/prac01.ts
+++ src/prac01.ts
@@ -1,6 +1,18 @@
 export {};
-const todo = {
-  name: "TypeScriptの勉強", // name = "..." ではない点に要注意
-  priority: 3, // priority = "..." ではない点に要注意
-};
-console.log(`Todo 1 => ${todo.name}（優先度:${todo.priority})`);
+
+// 関数の定義
+function date2str(dt: Date): string {
+  const year = dt.getFullYear();
+  const month = String(dt.getMonth() + 1).padStart(2, "0");
+  const day = String(dt.getDate()).padStart(2, "0");
+  const hours = String(dt.getHours()).padStart(2, "0");
+  const minutes = String(dt.getMinutes()).padStart(2, "0");
+  return `${year}/${month}/${day} ${hours}:${minutes}`;
+}
+
+const deadline: Date = new Date(2024, 10, 2, 11, 45);
+const cratedAt: Date = new Date(); // 引数なしで現在日時を取得
+
+// 関数の呼出し (テンプレート文字列の内部)
+let str = `期限 ${date2str(deadline)} (登録日 ${date2str(cratedAt)})`;
+console.log(str);
