fe637257c74c9694746bf9a8cc24dca7d3b9af79
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-10-10 10:10:20 +0900

    build(deps): update package-lock.json, package.json and prac01.ts

--- src/prac01.ts
+++ src/prac01.ts
@@ -1,18 +1,14 @@
 export {};
+import dayjs from "dayjs";
 
-// 関数の定義
-function date2str(dt: Date): string {
-  const year = dt.getFullYear();
-  const month = String(dt.getMonth() + 1).padStart(2, "0");
-  const day = String(dt.getDate()).padStart(2, "0");
-  const hours = String(dt.getHours()).padStart(2, "0");
-  const minutes = String(dt.getMinutes()).padStart(2, "0");
-  return `${year}/${month}/${day} ${hours}:${minutes}`;
-}
+import "dayjs/locale/ja";
+dayjs.locale("ja");
 
+const dtFmt = "YYYY/MM/DD(ddd) HH:mm";
 const deadline: Date = new Date(2024, 10, 2, 11, 45);
 const cratedAt: Date = new Date(); // 引数なしで現在日時を取得
 
-// 関数の呼出し (テンプレート文字列の内部)
-let str = `期限 ${date2str(deadline)} (登録日 ${date2str(cratedAt)})`;
+const str =
+  `期限 ${dayjs(deadline).format(dtFmt)}` +
+  `(登録日 ${dayjs(cratedAt).format(dtFmt)})`;
 console.log(str);
